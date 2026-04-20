404668125f07b80230612d6ee9ec91d01412e425
David Sankel <dsankel@adobe.com>
2026-04-20 15:52:02 -0400

    Use local zngur executable, format generated file, and use C++20 for build on Windows.

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 if not exist "src" mkdir src
-zngur generate main.zng
+..\zngur\target\debug\zngur.exe generate main.zng
+clang-format -i generated.h
 cargo build
-clang++ -I. main.cpp target/debug/png_example.lib -lws2_32 -luserenv -lntdll
+clang++ -std=c++20 -I. main.cpp target/debug/png_example.lib -lws2_32 -luserenv -lntdll
